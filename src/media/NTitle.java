b61effeac55c720c77179a282981a61a67fb5ea9
maicondublin <maicondublin@gmail.com>
2020-05-09 16:52:23 +0100

    NTitle done

--- src/media/NTitle.java
+++ src/media/NTitle.java
@@ -7,6 +7,25 @@ package media;
  * @author maiconalmeida
  *
  */
-public class NTitle {
+public abstract class NTitle extends MediaTitle { //obtian elements of media title class
+	
+	//storing data from a user inputs
+	static String title;
+	static String year;
+	
+	
+	//getting input from a user 
+	public static String getTitle() {
+		return title;
+	}
+	public static void setTitle(String title) {
+		NTitle.title = title;
+	}
+	public static String getYear() {
+		return year;
+	}
+	public static void setYear(String year) {
+		NTitle.year = year;
+	}
 
 }
