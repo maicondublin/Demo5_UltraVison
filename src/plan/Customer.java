771bcf9feb4db5f172c997d86d1e17b7b627064b
maicondublin <maicondublin@gmail.com>
2020-05-09 19:50:51 +0100

    Customer class done

--- src/plan/Customer.java
+++ src/plan/Customer.java
@@ -8,5 +8,36 @@ package plan;
  *
  */
 public class Customer {
-
+	
+	//variables
+	protected String name;
+	protected String plan;
+	protected String credcard;
+	protected int points;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPlan() {
+		return plan;
+	}
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+	public String getCredcard() {
+		return credcard;
+	}
+	public void setCredcard(String credcard) {
+		this.credcard = credcard;
+	}
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	
 }
