5cc5e35dc9c1d22c441b4f0fecaaa141a18089bf
maicondublin <maicondublin@gmail.com>
2020-05-09 20:08:45 +0100

    VL done

--- src/plan/VL.java
+++ src/plan/VL.java
@@ -7,6 +7,16 @@ package plan;
  * @author maiconalmeida
  *
  */
-public class VL {
+public class VL extends Customer { //VL [Video Lovers]
+	
+	
+	public VL(String name, String plan, String credcard, int points) {
+		
+		this.name = name;
+		this.plan = "VL";
+		this.credcard = credcard;
+		this.points = 1;
+		
+	}
 
 }
