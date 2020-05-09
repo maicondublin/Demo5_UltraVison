c022a8239b34107c10b7e300550a5d0a53c45426
maicondublin <maicondublin@gmail.com>
2020-05-09 20:04:43 +0100

    TVL done

--- src/plan/TVL.java
+++ src/plan/TVL.java
@@ -7,6 +7,16 @@ package plan;
  * @author maiconalmeida
  *
  */
-public class TVL {
+public class TVL  extends Customer{ //TVL [TV LOVERs]
+	
+	
+	public TVL(String name, String plan, String credcard, int points) {
+		
+		
+		this.name = name;
+		this.plan = "TV";
+		this.credcard = credcard;
+		this.points = 1;
+	}
 
 }
