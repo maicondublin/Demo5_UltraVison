752d80b3cbade620dff0915ca81eef204a908976
maicondublin <maicondublin@gmail.com>
2020-05-09 16:12:48 +0100

    Boxset done
    this class extends from NTitle [new title]

--- src/media/Boxset.java
+++ src/media/Boxset.java
@@ -7,6 +7,22 @@ package media;
  * @author maiconalmeida
  *
  */
-public class Boxset {
+public class Boxset  extends NTitle{//boxset is obtain elements from NTile class
+	
+	public Boxset(String title, String year) {
+		
+	    NTitle.title = title;
+		NTitle.year = year;
+		
+		
+	}
 
-}
+	@Override
+	public String toString() {
+		//print out a new title added
+		return "New Title successfuly added" + title + "director" + year;
+	}
+		
+	}
+
+
