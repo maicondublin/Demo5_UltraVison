58638ed842c356b5782ab53fa646a06b551de6e9
maicondublin <maicondublin@gmail.com>
2020-05-09 19:59:32 +0100

    ML done

--- src/plan/ML.java
+++ src/plan/ML.java
@@ -7,6 +7,20 @@ package plan;
  * @author maiconalmeida
  *
  */
-public class ML {
+public class ML extends Customer{// ML [music lovers]
+	
+	public ML (String name, String plan, String credcard, int points) {
+		
+		this.name = name;
+		this.plan = "ML";
+		this.credcard = credcard;
+		this.points = 1;
+	}
+	
+	public String toString() {
+		//print out a new customer saved into a DB
+		return("Customer was successfully recorded" + name + "credcard number " + credcard);
+		
+	}
 
 }
