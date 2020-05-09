1a189967ab4658d8960adf4a52d1de35a57d3b9f
Washington Bertolino <wash.bsilva@gmail.com>
2020-05-09 16:14:24 +0100

    Class Lconcert
    Lconcert done

--- src/media/Lconcert.java
+++ src/media/Lconcert.java
@@ -1,5 +1,23 @@
 package media;
 
-public class Lconcert {
+// @author washington
 
+public class Lconcert extends NTitle { // Lconcert is taking elements from a NTitle class 
+
+	public Lconcert (String title, String year, String set, String band) {
+		
+	//recording data from a user input
+		NTitle.title = title;
+		NTitle.year = year;
+		//this.set = set;
+		NTitle.band = band;
+		
+	}
+	@Override
+	public String to String() {
+		//print out the live concerts input
+		return "New Live Concert / Music successfully recorded " + title + " Band" + band;
+		
+		
+	}
 }
